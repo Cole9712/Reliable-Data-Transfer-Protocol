8dd5be049a58aaa612ce4a3d3009b00fd7c96944
Cole Hao Wu <hwa129@sfu.ca>
2021-04-12 17:08:17 -0700

    bug fix

--- receiver.py
+++ receiver.py
@@ -71,7 +71,7 @@ class RecvServer(object):
                 self.sendACK(remote_addr)
                 self.file.write(self.rcvBuffer[j].data)
                 self.packetCount += 1
-                print('Writting packet No.{0} into destination file'.format(self.packetCount))
+                print('Writting packet No.{0}, seqNum {1} into destination file'.format(self.packetCount, seqNum))
             
                 j += 1
 
